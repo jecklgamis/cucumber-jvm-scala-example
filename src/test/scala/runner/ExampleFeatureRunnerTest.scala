f187b20f4f6da248d3f9a6c54b8d71ad4217d9ce
Jerrico Gamis <jecklgamis@gmail.com>
2017-11-17 21:15:17 +1100

    Update to scala-2.12.4, cucumber-2.0.1; replace ~ with "not" in tag

--- src/test/scala/runner/ExampleFeatureRunnerTest.scala
+++ src/test/scala/runner/ExampleFeatureRunnerTest.scala
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith
 @RunWith(classOf[Cucumber])
 @CucumberOptions(
   features = Array("classpath:features/Example.feature"),
-  tags = Array("~@Wip"),
+  tags = Array("not @Wip"),
   glue = Array("classpath:steps"),
   plugin = Array("pretty", "html:target/cucumber/html"))
 class ExampleFeatureRunnerTest
