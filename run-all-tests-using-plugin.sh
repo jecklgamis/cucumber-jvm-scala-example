bcc47903eec5c16ef8c300994fd9c0294413831d
Jerrico Gamis <jecklgamis@gmail.com>
2017-11-17 21:20:02 +1100

    Fix more  ~ with "not" in tags

--- run-all-tests-using-plugin.sh
+++ run-all-tests-using-plugin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 PLUGINS="--plugin pretty --plugin html:cucumber/html --plugin json:cucumber/json/cucumber.json"
-mvn exec:java -Dcucumber.options="${PLUGINS} --tags ~@Wip --tags @ExampleFeature --glue steps classpath:features"
+mvn exec:java -Dcucumber.options="${PLUGINS} --tags not @Wip --tags @ExampleFeature --glue steps classpath:features"
